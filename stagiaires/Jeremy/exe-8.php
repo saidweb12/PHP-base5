ee4a57b8aaf53127d61589b7ef24e6a4f0a2b1e3
jeremyvdk91 <vanderkelen.jer@gmail.com>
2025-01-10 12:08:54 +0100

    exe-8

---
+++ stagiaires/Jeremy/exe-8.php
@@ -0,0 +1,16 @@
+<?php
+/*
+PHP utilise les tableaux pour les variables d'environnement
+comme les variables GET($_GET []).
+On va observer la variable $_GEt grace à var_dum()
+et ajout de $_POST
+
+*/
+var_dump($_GET,$_POST);
+?>
+<a href="?">Accueil</a>
+<a href="?p=1">variable get p vaut 1</a>
+<a href="?p=7">variable get p vaut 7</a>
+<a href="?p=1&search=coucou">variable get p vaut 1</a>
+<p>par défault les variables d'environement sont des tableaux associatifs
+en PHP!</p>
