1e88822d85a223b5b2d74372c15fadb9e674d83d
mikhawa <michaeljpitz@gmail.com>
2025-01-13 18:41:55 +0100

    exe3

--- stagiaires/michael/03-hello-friends.php
+++ stagiaires/michael/03-hello-friends.php
@@ -3,8 +3,30 @@
 
 echo "Hello World";
 echo "<br>Hello friends";
+$echo = "Hello";
+$if = "World";
+//
+$addition = (1+25)/2;
+$array = [1,2,3,4,5];
+$myArray = [
+    "prenom" => "Omer",
+    "classe" => "Classe 1",
+];
 ?>
 <h1>Coucou</h1>
 <p>Année: <?=date("Y-m-d H:i:s")?></p>
+<p><?="$echo $if"?></p>
+<p><?='$echo $if'?></p>
+<p><?=$echo." ".$if?></p>
+<p><?=$echo.' '.$if." = ".$addition?></p>
+<p><?=$echo,' ',$if?></p>
+<p><?=$echo.$if?></p>
+<?php
+echo "<p>$array[0]</p><p>$array[1]</p>";
+echo "<p>$myArray[prenom] - $myArray[classe]</p>";
+echo "<p>{$myArray['prenom']} - {$myArray['classe']}</p>";
+echo '<p>{$myArray["prenom"]} - {$myArray["classe"]}</p>';
+echo '<p>{$myArray[\'prenom\']} - {$myArray[\'classe\']}</p>';
 
-
+?>
+<p><?php var_dump($array,$myArray )?></p>
---
+++ stagiaires/michael/testhtml.php
@@ -0,0 +1,16 @@
+<?php
+$date = date("Y-m-d H:i:s");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Titre | <?=$date?></title>
+</head>
+<body>
+<div>
+    <h1>Titre | <?=$date?> | <?=rand(10,20)?></h1>
+</div>
+</body>
+</html>
