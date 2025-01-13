b9ed1fbd72f82d5d6395cedccc3a2cfef112eb32
mikhawa <michaeljpitz@gmail.com>
2025-01-13 14:46:45 +0100

    exe4

---
+++ stagiaires/michael/04-commentaires.php
@@ -0,0 +1,21 @@
+<?php
+// commentaire sur une ligne
+
+echo "coucou";// autre commentaire sur une ligne
+
+
+# autre commentaire sur une ligne
+
+/*
+ * Commentaires sur
+  plusieurs lignes
+ *
+ */
+
+/**
+ * Attention ce n'est pas qu'un commentaire !
+ * On appelle ça une annotation, et ça peut avoir une
+ * utilité autre que simplement commenter
+ * Par exemple PHPDOC
+ * @author Pitz Michaël
+ */
