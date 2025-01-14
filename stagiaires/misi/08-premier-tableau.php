4e72a4ebc7ec284ccce320b95db9d130a7fd6a5e
Misikalenga <misikalenga@gmail.com>
2025-01-14 13:45:44 +0100

    exercice 8

---
+++ stagiaires/misi/08-premier-tableau.php
@@ -0,0 +1,11 @@
+<?php
+// Tableau indexé
+$voitures = array("Volvo", "BMW", "Toyota");
+// Tableau indexé en raccourci
+$langages = ["PHP", "MySQL", "JavaScript"];
+// Tableau prenom eleve de la classe
+$prenom = ["saïd", "Daniel", "Mykyta", "Géraldine", "Reda", "Samuel", "Jeremy", "JM", "Sola"];
+
+// affichage des tableaux via le débugueur var_dump()
+var_dump($voitures, $langages, $prenom);
+?>
