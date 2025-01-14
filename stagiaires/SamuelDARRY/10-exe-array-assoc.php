e98362b43e1fe6e0ca7208cbeb15a93885339d98
Eultype <Sdarryy59@gmail.com>
2025-01-14 15:56:57 +0100

    10-exe-array-assoc.php

---
+++ stagiaires/SamuelDARRY/10-exe-array-assoc.php
@@ -0,0 +1,10 @@
+<?php
+
+$monTabAssoc = [
+    "prenom" => "Samuël", 
+    "nom" =>  "DARRY", 
+    "caracteristique" => "gamer", 
+    "dateDeNaissance" => "1995-03-14"
+];
+
+var_dump($monTabAssoc);
