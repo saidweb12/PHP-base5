9fc14fadfb6659dc84902742979d86c6d6cbb75c
GEUNGA <ungaro.geraldine@gmail.com>
2025-01-14 16:00:45 +0100

    exe10

---
+++ stagiaires/geraldineungaro/10-exe-array-assoc.php
@@ -0,0 +1,12 @@
+<?php
+
+$monTabAssoc = [
+    "prénom" => "Michael",
+    "nom" => "Pitz",
+    "caractéristique" => "égocentrique",
+    "date de naissance" => "1951-03-02"
+];
+
+    foreach($monTabAssoc as $x => $y){
+        echo "$x = $y <hr>";
+    };
