9d17107fa9aea5ce0b51a2e41aebf11ceaab58cd
vahag1991 <smbatyanvahagn1991@gmail.com>
2025-01-15 12:00:32 +0100

    exe9b

---
+++ stagiaires/vahag/08-premier-tableau.php
@@ -0,0 +1,17 @@
+<?php
+$classe1 = array(
+    "Omer",
+    "Agim",
+    "Massine",
+    "Loukas",
+    "Soulaiman",
+    "Nordine",
+    "Vahagn",
+);
+
+var_dump($classe1);
+
+
+echo $classe1 [0]." ". $classe1[1];
+echo "<hr>";
+
