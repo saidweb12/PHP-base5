e5ae94d3b8c8e8f991d2e7f3e1259dcb6fa09003
user <earn_arctic.0k@icloud.com>
2025-01-20 16:02:59 +0100

    JM-12-GET.php

---
+++ stagiaires/Jean-Michael/JM-12-GET.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="JM-12-GET.php">Accueil</a>
+        <a href="?p=1">PageUne</a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+    </nav>
+<?php
+// affiche le contenu de la variable superglobale $_GET
+var_dump($_GET);
+?>
+</body>
+</html>
