21522088c7e9bc6874e182400ac3d0ec0d85e1e6
vahag1991 <smbatyanvahagn1991@gmail.com>
2025-01-27 14:15:58 +0100

    exe10

---
+++ stagiaires/vahag/10-exe-array-assoc.php
@@ -0,0 +1,18 @@
+<?php
+// tableau associatif
+$monTabAssoc = ["prenom" => "Vahagn", "nom" => "Smbatyan", "caracteristique" => "charmeur","dateDeNaissance" => "1991-07-21"];
+echo $monTabAssoc ["prenom"]. "<hr>"; 
+echo $monTabAssoc ["nom"]. "<hr>"; 
+echo $monTabAssoc ["caracteristique"]. "<hr>"; 
+echo $monTabAssoc ["dateDeNaissance"]. "<hr>"; 
+
+
+
+
+
+
+
+
+
+
+?>
