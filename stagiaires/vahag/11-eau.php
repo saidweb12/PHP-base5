90d0bb7121e401002daa410ee52335eb066daf53
Formateurs Web <146428844+WebDevCF2m@users.noreply.github.com>
2025-01-27 15:42:16 +0100

    Update 11-eau.php

--- stagiaires/vahag/11-eau.php
+++ stagiaires/vahag/11-eau.php
@@ -1,11 +1,11 @@
 <?php
-$temperatur = 200; 
-if ($temperatur<= 0){
-    echo "l'état de l'eau est solide";
-}elseif ($temperatur> 0 && $temperatur< 99){
-    echo "l'état de l'eau est liquide";
+$temperature = mt_rand(-100,200); 
+if ($temperature<= 0){
+    echo "l'état de l'eau est solide à $temperature °C";
+}elseif ($temperature> 0 && $temperature<= 99){
+    echo "l'état de l'eau est liquide à $temperature °C";
 }else {
-    echo "l'état de l'eau est gazeux";
+    echo "l'état de l'eau est gazeux à $temperature °C";
 }
 
 
