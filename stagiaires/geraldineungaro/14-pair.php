a33c5c2aa99251b28de27f0eb85f03a2964c5b17
GEUNGA <ungaro.geraldine@gmail.com>
2025-01-30 16:07:05 +0100

    13

---
+++ stagiaires/geraldineungaro/14-pair.php
@@ -0,0 +1,11 @@
+<?php
+
+$a=rand(5,6);
+if($a%2==0){
+    echo "Le nombre est paire";
+}
+else{
+    echo "Le nombre est impaire";
+}
+
+echo $a;
---
+++ stagiaires/geraldineungaro/15b-switch.php
@@ -0,0 +1,18 @@
+<?php
+
+switch($a=rand(0,10)){
+case "1"($a<=3):
+    echo " Nul, étudie la prochaine fois";
+    break;
+case "2"($a<=5):
+    echo "Peut mieux faire";
+    break;
+case "3" ($a<=7):
+    echo "Bien";
+    break;
+case "4" :
+    echo "Très bien";
+    break;
+}
+
+echo $a;
