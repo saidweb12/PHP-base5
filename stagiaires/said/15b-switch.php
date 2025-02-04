14fef8f5d9245493cd2d6484acc1efb0158ed826
saidweb12 <said-farah@hotmail.com>
2025-02-04 14:19:38 +0100

    ex15b

---
+++ stagiaires/said/15b-switch.php
@@ -0,0 +1,40 @@
+<?php
+$rand = mt_rand (0,10);
+switch ($rand){
+
+    case 0: 
+    case 1:
+    case 2:
+    case 3:
+        echo "$rand : Nul , etudie la prochaine fois ";
+        break; 
+        
+    case 4: 
+    case 5: 
+        echo "$rand : peut mieux faire";
+        break;
+
+    case 6:      
+    case 7: 
+        echo "$ rand : bien ";
+        break;
+
+    defaut:
+        echo "$rand : tres bien " ;   
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
