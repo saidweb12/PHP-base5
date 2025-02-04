89b0e87c06f9525aa4e75a76a9f27117ab3bf7b8
saidweb12 <said-farah@hotmail.com>
2025-02-04 13:31:56 +0100

    ex15a

--- stagiaires/said/15-conditions.php
+++ stagiaires/said/15-conditions.php
@@ -2,15 +2,15 @@
 
  $nombre = mt_rand(0,10);
 
-if( $nombre >=0 && $nombre <=3){
+if(  $nombre <=3){
 
     echo "$nombre, nul etudie la prochaois fois";
 
-}elseif ($nombre>= 4 && $nombre <=5){
+}elseif ($nombre <=5){
 
    echo " $nombre , peut mieux faire " ;
 
-}elseif ($nombre>= 6 && $nombre <=7){
+}elseif ( $nombre <=7){
 
     echo " $nombre, bien";
 
